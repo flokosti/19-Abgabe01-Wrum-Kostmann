dcb9f6db816e6e593c7027173494fd42042fcbb3
Florian Kostmann <florian.kostmann@edu.fh-joanneum.at>
2019-03-29 11:51:16 +0100

    Initial commit. Added project structure, added addValue and empty min, max methods in CalculatorImpl Class

---
+++ src/main/java/CalculatorImpl.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class CalculatorImpl implements Calculator {
+
+    //Arraylist to save values
+    ArrayList<Integer> values=new ArrayList<Integer>();
+
+    public int sum() {
+        return 0;
+    }
+
+    public int getMaximum() {
+        return 0;
+    }
+
+    public int getMinimum() {
+        return 0;
+    }
+
+    //Method to add int values to arraylist
+    public void addValue(int value) {
+        values.add(value);
+
+    }
+}
